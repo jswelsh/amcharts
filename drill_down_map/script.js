104928e5f41692c04146af6b86abb667b1f781cb
jswelsh <james.sinclair.welsh@gmail.com>
2020-10-23 02:38:35 -0700

    changed the variable setting

--- drill_down_map/script.js
+++ drill_down_map/script.js
@@ -1,15 +1,3 @@
-/**
- * ---------------------------------------
- * This demo was created using amCharts 4.
- * 
- * For more information visit:
- * https://www.amcharts.com/
- * 
- * Documentation is available at:
- * https://www.amcharts.com/docs/v4/
- * ---------------------------------------
- */
-
 const continents = {
   "AF": 0,
   "AN": 1,
@@ -24,28 +12,31 @@ const continents = {
 am4core.useTheme(am4themes_animated);
 
 /* Create map instance */
-let chart = am4core.create("chartdiv", am4maps.MapChart);
+const chart = am4core.create("chartdiv", am4maps.MapChart);
 chart.projection = new am4maps.projections.Miller();
 
 // Create map polygon series for world map
-let worldSeries = chart.series.push(new am4maps.MapPolygonSeries());
+const worldSeries = chart.series.push(new am4maps.MapPolygonSeries());
 worldSeries.useGeodata = true;
 worldSeries.geodata = am4geodata_worldLow;
 worldSeries.exclude = ["AQ"];
 
-let  worldPolygon = worldSeries.mapPolygons.template;
+const  worldPolygon = worldSeries.mapPolygons.template;
 worldPolygon.tooltipText = "{name}";
 worldPolygon.nonScalingStroke = true;
 worldPolygon.strokeOpacity = 0.5;
 worldPolygon.fill = am4core.color("#eee");
 worldPolygon.propertyFields.fill = "color";
 
-var hs = worldPolygon.states.create("hover");
-hs.properties.fill = chart.colors.getIndex(9);
+worldPolygon
+  .states
+  .create("hover")
+  .properties
+  .fill = chart.colors.getIndex(9);
 
 
 // Create country specific series (but hide it for now)
-let countrySeries = chart.series.push(new am4maps.MapPolygonSeries());
+const countrySeries = chart.series.push(new am4maps.MapPolygonSeries());
 countrySeries.useGeodata = true;
 countrySeries.hide();
 countrySeries.geodataSource.events.on("done", function(ev) {
@@ -53,22 +44,25 @@ countrySeries.geodataSource.events.on("done", function(ev) {
   countrySeries.show();
 });
 
-let countryPolygon = countrySeries.mapPolygons.template;
+const countryPolygon = countrySeries.mapPolygons.template;
 countryPolygon.tooltipText = "{name}";
 countryPolygon.nonScalingStroke = true;
 countryPolygon.strokeOpacity = 0.5;
 countryPolygon.fill = am4core.color("#eee");
 
-var hs = countryPolygon.states.create("hover");
-hs.properties.fill = chart.colors.getIndex(9);
+countryPolygon
+  .states
+  .create("hover")
+  .properties
+  .fill = chart.colors.getIndex(9);
 // Set up click events
 worldPolygon.events.on("hit", function(ev) {
   drillDown(ev.target)
 });
 
-function drillDown(target) {
+const drillDown = (target) => {
   target.series.chart.zoomToMapObject(target);
-  var map = target.dataItem.dataContext.map;
+  const map = target.dataItem.dataContext.map;
   if (map) {
     target.isHover = false;
     countrySeries.geodataSource.url = "https://cdn.amcharts.com/lib/4/geodata/json/" + map + ".json";
@@ -78,10 +72,10 @@ function drillDown(target) {
 }
 
 // Set up data for countries
-var data = [];
-for(var id in am4geodata_data_countries2) {
+const data = [];
+for(const id in am4geodata_data_countries2) {
   if (am4geodata_data_countries2.hasOwnProperty(id)) {
-    var country = am4geodata_data_countries2[id];
+    const country = am4geodata_data_countries2[id];
     if (country.maps.length) {
       data.push({
         id: id,
@@ -94,7 +88,7 @@ for(var id in am4geodata_data_countries2) {
 worldSeries.data = data;
 
 // Add zoomout button
-var back = chart.createChild(am4core.ZoomOutButton);
+const back = chart.createChild(am4core.ZoomOutButton);
 back.align = "right";
 back.hide();
 back.events.on("hit", function(ev) {
@@ -105,7 +99,7 @@ back.events.on("hit", function(ev) {
 });
 
 
-var currentHover;
+let currentHover;
 
 worldPolygon.events.on("over", function(ev) {
   currentHover = ev.target;
